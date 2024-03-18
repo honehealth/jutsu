9b6833a2aa922d14376a63133d73f50eb62e88dc
greym0uth <thejadenjack@gmail.com>
2024-03-18 16:20:53 -0500

    fix disabled and hover styles

--- src/components/Input.ts
+++ src/components/Input.ts
@@ -7,6 +7,13 @@ export default {
       field: {
         borderRadius: 0,
         borderColor: 'gray.600',
+        _disabled: {
+          opacity: 1,
+          color: 'gray.400',
+        },
+        _hover: {
+          borderColor: 'gray.600',
+        }
       },
     },
     filled: {
--- src/components/Select.ts
+++ src/components/Select.ts
@@ -2,10 +2,21 @@ import { ComponentStyleConfig } from '@chakra-ui/react';
 
 export default {
   parts: ['field'],
+  baseStyle: {
+    border: '1px solid',
+  },
   variants: {
     outline: {
       field: {
         borderRadius: 0,
+        borderColor: 'gray.600',
+        _disabled: {
+          opacity: 1,
+          color: 'gray.400',
+        },
+        _hover: {
+          borderColor: 'gray.600',
+        },
       },
     },
     filled: {
