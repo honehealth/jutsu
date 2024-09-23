e207796eceac940626b2902b6123c74b3be74353
greym0uth <thejadenjack@gmail.com>
2024-09-23 12:32:17 -0500

    add shadow levels

--- src/index.ts
+++ src/index.ts
@@ -22,5 +22,14 @@ export default extendTheme({
       width: '100vw',
     },
   },
+  shadows: {
+    xs: '0px 2px 4px 0px rgba(234, 234, 234, 0.1)',
+    sm: '0px 2px 8px 0px rgba(234, 234, 234, 0.2)',
+    base: '0px 4px 16px 0px rgba(234, 234, 234, 0.25)',
+    md: '0px 4px 16px 0px rgba(234, 234, 234, 0.25)',
+    lg: '0px 4px 16px 0px rgba(234, 234, 234, 0.25)',
+    xl: '0px 4px 16px 0px rgba(234, 234, 234, 0.25)',
+    '2xl': '0px 4px 16px 0px rgba(234, 234, 234, 0.25)',
+  },
   useSystemColorMode: false,
 } as ThemeConfig);
